2af97cf1aa721e912e63a680a1ef2f268c997fba
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2026-02-19 14:54:52 +0100

    add @vee-validate/nuxt and @vee-validate/rules dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,26 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@sidebase/nuxt-auth", "@nuxt/icon"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@sidebase/nuxt-auth",
+    "@nuxt/icon",
+    "@vee-validate/nuxt",
+  ],
 
   vite: {
     plugins: [[tsconfigPaths()]],
+  },
+
+  veeValidate: {
+    autoImports: true,
+
+    componentNames: {
+      Field: "VField",
+      Form: "VForm",
+      FieldArray: "VFieldArray",
+      ErrorMessage: "VErrorMessage",
+    },
   },
 
   auth: {
