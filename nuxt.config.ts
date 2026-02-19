195381c85f83705b18cb8449abb989707d34ace2
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2026-02-19 17:28:35 +0100

    remove: eliminate @sidebase/nuxt-auth dependency from project to replace with http cookies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     "@nuxtjs/tailwindcss",
-    "@sidebase/nuxt-auth",
     "@nuxt/icon",
     "@vee-validate/nuxt",
   ],
@@ -23,50 +22,6 @@ export default defineNuxtConfig({
       Form: "VForm",
       FieldArray: "VFieldArray",
       ErrorMessage: "VErrorMessage",
-    },
-  },
-
-  auth: {
-    isEnabled: true,
-    originEnvKey: "NUXT_BASE_URL",
-    baseURL: "http://localhost:3000/api",
-
-    provider: {
-      type: "local",
-
-      endpoints: {
-        signIn: { path: "/login", method: "post" },
-        signOut: { path: "/logout", method: "post" },
-        signUp: { path: "/register", method: "post" },
-        getSession: { path: "/session", method: "get" },
-      },
-
-      refresh: {
-        isEnabled: true,
-        endpoint: { path: "/refresh", method: "post" },
-        refreshOnlyToken: true,
-      },
-
-      token: {
-        signInResponseTokenPointer: "Token",
-        type: "Bearer",
-        cookieName: "auth_token",
-        headerName: "Authorization",
-        maxAgeInSeconds: 1800,
-        sameSiteAttribute: "lax",
-        cookieDomain: "sidebase.io",
-        secureCookieAttribute: false,
-        httpOnlyCookieAttribute: false,
-      },
-
-      pages: {
-        login: "/auth/login",
-      },
-    },
-
-    sessionRefresh: {
-      enableOnWindowFocus: true,
-      enablePeriodically: true,
     },
   },
 });
