e276353945281c2891c186c91b8027dffeeb1e12
pgm-lukaverv <lukas.vervaet@student.arteveldehs.be>
2026-02-24 15:17:52 +0100

    display navbar on ALL pages besides the auth & profile creation pages

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <!-- Hide Navbar on auth pages and profile creation -->
+    <Navbar
+      v-if="!route.path.startsWith('/auth') && route.path !== '/profile/create'"
+    />
+
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+</script>
